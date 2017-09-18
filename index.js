d169bfa8287fa99d54cb3727e1a82a3402d0e5fd
Lucas Andre Dias Cavalheiro <lucas@Lucass-MacBook-Air.local>
2017-09-18 03:11:48 -0300

    change api port

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var bot = require('./lib/bot/main.js');
 
 function init() {
+  process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+  });
   bot.initBot();
 }
 
